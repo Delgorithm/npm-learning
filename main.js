c19db7160a8df3f88da1b8b3b471d424b1a18a46
Delgorithm <delgorithm@gmail.com>
2023-05-16 14:47:48 +0200

    V1.0.2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var moment = require("moment");
+var unique = require('uniq');
 
 var myDate = new Date();
-var myCurrentDate = moment(myDate).format("YYYY [escaped] YYYY");
+var myCurrentDate = moment(myDate).format("LL");
 
 console.log(myCurrentDate);
