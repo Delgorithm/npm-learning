4aba3443fa0ca8d7dc1c2f278bceb27d27f72940
Delgorithm <delgorithm@gmail.com>
2023-05-16 14:50:54 +0200

    V1.0.3

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ var unique = require('uniq');
 var myDate = new Date();
 var myCurrentDate = moment(myDate).format("LL");
 
-console.log(myCurrentDate);
+var myList = [1,2,1,1,1,3,3,6,5,5,3,4,6];
+
+var myUniqueList = unique(myList);
+
+console.log(myUniqueList);
